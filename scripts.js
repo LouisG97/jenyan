8c379fb081e9632622f0e8828ce30c343a3f1fe0
José Luis Gómez Figueroa <j_k_h@live.com.mx>
2024-07-07 12:11:26 -0600

    First commit
    This is the first commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function navigateTo(section) {
+    // Navigate to different sections based on the image clicked
+    switch (section) {
+        case 'pareja':
+            window.location.href = '#pareja';
+            break;
+        case 'embarazo':
+            window.location.href = '#embarazo';
+            break;
+        case 'bebe':
+            window.location.href = '#bebe';
+            break;
+    }
+}
